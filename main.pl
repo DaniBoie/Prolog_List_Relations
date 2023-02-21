d8cae17fbaf9f8cf7556e346c15493bc82cb62ba
Daniel Ayala <dannyboy3385@gmail.com>
2023-02-21 09:41:32 -0800

    my-length

--- main.pl
+++ main.pl
@@ -11,3 +11,9 @@ time(volvo, 24).
 distance(X, Y) :- speed(X, Speed),
  time(X, Time),
  Y is Speed * Time.
+
+% Question 1: Define a relation my_length that takes two arguments, a list L, and an argument, R, for the number of top_level elements in L.
+
+my_length([], 0).
+
+my_length([Head|Tail], Length) :- Length is 1 + my_length(tail, _).
